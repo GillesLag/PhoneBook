f71e489650edcd98de19c98f1d14cc99fef499e1
Gilles Lagrilliere <gilles.lagrilliere@codraft.be>
2025-01-31 13:25:11 +0100

    contactController added

---
+++ PhoneBook/Controllers/ContactController.cs
@@ -0,0 +1,45 @@
+﻿using PhoneBook.Data;
+using PhoneBook.Models;
+
+namespace PhoneBook.Controllers;
+
+public class ContactController
+{
+    private readonly PhoneBookContext _context;
+
+    public ContactController(PhoneBookContext context)
+    {
+        _context = context;
+    }
+    public int AddContact(Contact contact)
+    {
+        _context.Contacts.Add(contact);
+        return _context.SaveChanges();
+    }
+
+    public int RemoveContact(int id)
+    {
+        Contact contact = _context.Contacts.Find(id);
+
+        _context.Contacts.Remove(contact);
+
+        return _context.SaveChanges();
+    }
+
+    public int UpdateContact(Contact contact)
+    {
+        var entity = _context.Contacts.Find(contact.Id);
+
+        if (entity != null)
+        {
+            _context.Contacts.Update(contact);
+        }
+
+        return _context.SaveChanges();
+    }
+
+    public List<Contact> GetContacts()
+    {
+        return _context.Contacts.ToList();
+    }
+}
