c630d9997c98a20ab7825f04233b9236c27906e9
Gilles Lagrilliere <gilles.lagrilliere@codraft.be>
2025-01-31 12:58:56 +0100

    Created database

---
+++ PhoneBook/Data/PhoneBookContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Data;
+
+public class PhoneBookContext : DbContext
+{
+    private readonly string _connectionString;
+    public DbSet<Contact> Contacts { get; set; }
+    
+    public PhoneBookContext()
+    {
+        _connectionString = ConfigurationManager.AppSettings.Get("ConnectionString") ??
+            throw new Exception("Key value pair doesn't exist in the config-file!");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionString);
+    }
+}
